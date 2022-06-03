4e92f40cabc376bdeddeec29890d321287a3e200
smhrd <smhrd@DESKTOP-UKIGL2R>
2022-06-03 15:43:29 +0900

    회원가입, 로그인 - 나윤

---
+++ SixMonthProject/src/main/java/com/model/MemberVO.java
@@ -0,0 +1,18 @@
+package com.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+ 
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberVO {
+	
+	// 회원정보 필드
+	private String id;
+	private String name;
+	private String pw;
+	private String email;
+	private String nick;
+}
